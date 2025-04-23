f646c7b83a67d36983c46f1ba9df5fa2b66da383
Kevin Vincent Loachamin Almeida <kevinvincentvl016@gmail.com>
2025-04-23 14:44:47 -0500

    aaa?

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,6 @@
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
-  version = "=3.0.0"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -31,36 +30,6 @@ resource "azurerm_public_ip" "public_ip" {
   sku                 = "Basic"
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-dev"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-App-Ports"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["8000", "8080", "8082", "8083"]
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
 resource "azurerm_network_interface" "nic" {
   name                = "nic-dev"
   location            = azurerm_resource_group.rg.location
@@ -72,8 +41,6 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
-  
-  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
