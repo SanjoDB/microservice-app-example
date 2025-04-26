7ccd7461315dc6f74f4967a0d61b6795a01c3db0
Kevin Vincent Loachamin Almeida <kevinvincentvl016@gmail.com>
2025-04-26 14:28:36 -0500

    Add static private ip

--- infra/main.tf
+++ infra/main.tf
@@ -39,6 +39,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.10"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
