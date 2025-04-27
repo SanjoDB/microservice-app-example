358aa97595cb2fdbcf64f02fc2966b4d773012d4
Kevin Vincent Loachamin Almeida <kevinvincentvl016@gmail.com>
2025-04-27 17:27:59 -0500

    Give permitions to Azureuser

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -7,6 +7,7 @@ sudo usermod -aG docker azureuser
 cd /home/azureuser
 
 git clone --branch infra/dev https://github.com/SanjoDB/microservice-app-example.git
+sudo chown -R azureuser:azureuser ~/microservice-app-example
 cd microservice-app-example
 
 sudo docker-compose -f docker-compose.yml up -d --build
