68c81cbb4df1384bbedea2e4f7e161ac82ffb7f9
Kevin Vincent Loachamin Almeida <kevinvincentvl016@gmail.com>
2025-04-26 21:08:43 -0500

    asdasda

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,6 +5,11 @@ sudo apt-get install -y docker.io docker-compose git
 sudo usermod -aG docker azureuser
 
 cd /home/azureuser
+
+if [ -d "microservice-app-example" ]; then
+  sudo rm -rf microservice-app-example
+fi
+
 git clone --branch infra/dev https://github.com/SanjoDB/microservice-app-example.git
 cd microservice-app-example
 
